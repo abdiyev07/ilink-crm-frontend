a379c66c370b597e8f96704c47c15a758469fe15
Abdiyev Shadiyar <abdiyev096@mail.ru>
2023-09-17 17:30:28 +0600

    fix: router history

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import routes from "@/router/routes";
 import store from "@/store";
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
 });
 
